057684b2e2b8bc7731fb3b4f6c9b4e5ff57fa8be
mutafransheska45-maker <mutafransheska45@gmail.com>
2026-09-03 14:29:25 +0200

    working on the styles

--- src/pages/DreamSaved.jsx
+++ src/pages/DreamSaved.jsx
@@ -3,32 +3,15 @@ import "../styling/DreamSaved.css";
 
 function DreamSaved() {
   const navigate = useNavigate();
-
   return (
     <div className="dream-saved-page">
-
       <div className="dream-saved-container">
-
         <h1>Dream saved!</h1>
-
-        <h2>
-          Your dream has been recorded, now
-          <br />
-          generating your interpretation...
-        </h2>
-
-        <p>This may take a few seconds...</p>
-
-        <button
-          onClick={() => navigate("/journal")}
-        >
-          View in Journal
-        </button>
-
+        <h2>Your dream has been recorded now<br /> getting your interpretation...</h2>
+        {/* <p>This may take a few seconds...</p> */}
+        <button onClick={() => navigate("/journal")}>View in Journal</button>
       </div>
-
     </div>
-  );
+  )
 }
-
-export default DreamSaved;
+export default DreamSaved
